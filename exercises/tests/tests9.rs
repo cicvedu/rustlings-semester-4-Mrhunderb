a3cdbac3c6c6bf9c48cae22abf204e8b26555bef
jiayuezh <studentzhang2002@163.com>
2024-07-07 11:21:26 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,6 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
@@ -36,7 +35,14 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
+        a
+    }
+
+    // The `extern` keyword is used to make the function addressable by name.
+    #[no_mangle]
+    extern "Rust" fn my_demo_function_alias(a: u32) -> u32 {
         a
     }
 }
